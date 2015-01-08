43337de365d9d56d824e24eb2b4e4e80fda3c38f
Chris <chris.kec.baum@gmail.com>
2015-01-08 18:11:21 -0500

    Externalize dehash

--- js/meta.js
+++
@@ -1,5 +0,0 @@
-var viewPortTag=document.createElement('meta');
-viewPortTag.id="viewport";
-viewPortTag.name = "viewport";
-viewPortTag.content = "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0";
-document.getElementsByTagName('head')[0].appendChild(viewPortTag);
